016987eabe27c9c026d7c5346439e08f3fa861f4
Jason Wright <jaswri43@gmail.com>
2025-11-08 01:43:19 -0500

    polished code

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <iomanip>
 #include <random>
+#include <chrono>
+#include <fstream>
 #include "KDTree.h"
 #include "QuadTree.h"
 #include "Point.h"
@@ -14,7 +16,27 @@ int main()
     //Creating vector to hold points that will be added to trees
     std::vector<Point> points;
 
-    //Generating random 1000 random points for testing
+    /*
+    //Reading Gainesville crime data from text file
+
+    std::ifstream infile("data.txt");
+    if (!infile) {
+        std::cerr << "Failed to open file." << std::endl;
+        return 1;
+    }
+
+    std::vector<Point> points;
+    double lon;
+    double lat
+
+    while (infile >> lon >> lat) {
+        points.push_back(Point(lon, lat);
+    }
+
+
+     */
+
+    //Generating random 100000 random points for testing
 
     // Gainesville bounding box
     double xmin = -82.50000;
@@ -34,10 +56,12 @@ int main()
     //Generate values
     std::vector<double> latitudes;
     std::vector<double> longitudes;
-    latitudes.reserve(1000);
-    longitudes.reserve(1000);
+    latitudes.reserve(100);
+    longitudes.reserve(100);
 
-    for (int i = 0; i < 1000; ++i)
+
+
+    for (int i = 0; i < 100; ++i)
     {
         double lat = latDist(gen);
         double lon = lonDist(gen);
@@ -52,15 +76,32 @@ int main()
         points.push_back(Point(lon, lat));
     }
 
+
     //Create KDTree
+    //Insert data into kd tree and record time
+    auto start_time = std::chrono::steady_clock::now();
     KDTree kd(points);
+    auto end_time = std::chrono::steady_clock::now();
+
+    auto kdInsertDuration = end_time - start_time;
+
+    //Create QuadTree
+    Rectangle boundary(-82.33, 29.65, 0.1, 0.1);
+    QuadTree qd(boundary, 4);
+
+    //Insert data into quad tree and record time
+    start_time = std::chrono::steady_clock::now();
+    for (Point& point : points) {
+        qd.insert(point);
+    }
+    end_time = std::chrono::steady_clock::now();
+
+    auto quadInsertDuration = end_time - start_time;
 
     //User input
     double inputlat;
     double inputlon;
     double radiusmult;
-
-    std::cout << "Crime Rate Query\n\n";
 
     std::cout << "Enter target latitude (between 29.55 and 29.75): ";
     std::cin >> inputlat;
@@ -72,9 +113,38 @@ int main()
     std::cin >> radiusmult;
 
     double radius = baserad * radiusmult;
+    double area = M_PI * radiusmult * radiusmult;
+
+    Point target(inputlon, inputlat);
+
+    //Run Operations on KDTree
+
+    start_time = std::chrono::steady_clock::now();
+    std::vector<Point> kdInRange = kd.rangeSearch(target, radius);
+    end_time = std::chrono::steady_clock::now();
+
+    auto kdSearchDuration = end_time - start_time;
+
+    int kdcrimes = kdInRange.size();
+
+    double kdcrimedensity = kdcrimes / area; //Measured in crimes per mile squared
+
+    //Run Operations on QuadTree
+    start_time = std::chrono::steady_clock::now();
+    std::vector<Point> quadInRange;
+    qd.query(Rectangle(target.x, target.y, radius, radius), quadInRange);
+    end_time = std::chrono::steady_clock::now();
+
+    auto quadSearchDuration = end_time - start_time;
+
+    int quadcrimes = quadInRange.size();
+
+    double quadcrimedensity = quadcrimes / area; //Measured in crimes per mile squared
 
     //Plot data points
     Plot2D plot;
+    plot.xlabel("Longitude");
+    plot.ylabel("Latitude");
     plot.drawPoints(longitudes, latitudes).lineColor("red").label("");  // Red points, no legend
 
     // Generate circle points
@@ -86,14 +156,29 @@ int main()
         y.push_back(inputlat + radius * std::sin(theta));
     }
 
-    plot.drawCurve(x ,y).lineColor("blue").label("");
+    plot.drawCurve(x ,y).lineColor("blue").label("").lineWidth(2);
 
     Figure fig({{plot}});
     fig.title("Points with Target Ring");
 
     Canvas canvas({{fig}});
-    canvas.size(1200, 1200);
+    canvas.size(1500, 1300);
     canvas.title("Custom Axes Canvas");
+
+    std::cout << "\nKD Tree Metrics:" << std::endl;
+    std::cout << "==================" << std::endl;
+    std::cout << "Crimes in given radius: " << kdcrimes << std::endl;
+    std::cout << "Crimes Density (crimes/mile^2): " << kdcrimedensity << std::endl;
+    std::cout << "Time to insert data into tree (ns): " << std::chrono::duration_cast<std::chrono::nanoseconds>(kdInsertDuration).count() << std::endl;
+    std::cout << "Time to determine crimes in radius (ns): " << std::chrono::duration_cast<std::chrono::nanoseconds>(kdSearchDuration).count() << std::endl;
+
+    std::cout << "\nQuad Tree Metrics:" << std::endl;
+    std::cout << "==================" << std::endl;
+    std::cout << "Crimes in given radius: " << quadcrimes << std::endl;
+    std::cout << "Crimes Density: " << quadcrimedensity << std::endl;
+    std::cout << "Time to insert data into tree (ns): " << std::chrono::duration_cast<std::chrono::nanoseconds>(quadInsertDuration).count() << std::endl;
+    std::cout << "Time to determine crimes in radius (ns): " << std::chrono::duration_cast<std::chrono::nanoseconds>(quadSearchDuration).count() << std::endl;
+
     canvas.show();
 
     return 0;
